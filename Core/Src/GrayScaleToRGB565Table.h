543fea8482bc335592584b7680caa80936bdb6de
charlte1453 <ipsingh.1256@gmail.com>
2025-04-06 22:53:17 +0530

    Initial working version: LCD and camera functional

---
+++ Core/Src/GrayScaleToRGB565Table.h
@@ -0,0 +1,17 @@
+/*
+ * GrayScaleToRGB565Table.h
+ *
+ *  Created on: Apr 6, 2025
+ *      Author: Yukirin
+ */
+
+#include "stdint.h"
+
+#ifndef SRC_GRAYSCALETORGB565TABLE_H_
+#define SRC_GRAYSCALETORGB565TABLE_H_
+
+extern const uint16_t RGB565[256];
+
+
+
+#endif /* SRC_GRAYSCALETORGB565TABLE_H_ */
---
+++ Debug/Core/Src/GrayScaleToRGB.d
@@ -0,0 +1,3 @@
+Core/Src/GrayScaleToRGB.o: ../Core/Src/GrayScaleToRGB.c \
+ ../Core/Src/GrayScaleToRGB565Table.h
+../Core/Src/GrayScaleToRGB565Table.h:
